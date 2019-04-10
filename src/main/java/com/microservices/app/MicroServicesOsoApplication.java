318a6f3fb39e70a25262d89abffb4a1d06e7d677
Alejandro Muñoz Osorio <alejandro.mnuoz@4thsource.com>
2019-04-10 09:12:24 -0500

    Cleaning more details

--- src/main/java/com/microservices/app/MicroServicesOsoApplication.java
+++ src/main/java/com/microservices/app/MicroServicesOsoApplication.java
@@ -3,7 +3,6 @@ package com.microservices.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-//jj
 
 @ComponentScan
 @SpringBootApplication
@@ -11,6 +10,6 @@ public class MicroServicesOsoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesOsoApplication.class, args);
-	}//hehe
+	}
 
 }
--- src/main/java/com/microservices/app/dummy/ExampleController.java
+++ src/main/java/com/microservices/app/dummy/ExampleController.java
@@ -1,4 +1,4 @@
-package com.microservices.app.example;
+package com.microservices.app.dummy;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
